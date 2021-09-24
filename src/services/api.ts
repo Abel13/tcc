77f4beccc489efb4fa69cab8b612611bbea13a4b
Abel <abel.o.d@outlook.com>
2021-09-24 05:08:49 -0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // baseURL: 'http://127.0.0.1:8080',
-  baseURL: 'https://sheep-finance.herokuapp.com',
+  baseURL: 'http://127.0.0.1:8080',
 });
 
 export default api;
