8786baeef9f1cd9fa37c1605debeb7f68dc50ff6
Abel Dutra <abel.o.d@outlook.com>
2021-09-24 05:37:37 -0300

    feat: [#31] - added .env to project

--- src/components/pages/Dashboard/index.tsx
+++ src/components/pages/Dashboard/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function index() {
   return (
     <div>
-      
+      DASHBOARD
     </div>
   );
 }
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8080',
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 export default api;
