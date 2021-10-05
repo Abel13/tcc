7f9c781a2cc3f8d54e75ef528b0b1590e04f2fac
Abel Dutra <abel.o.d@outlook.com>
2021-10-05 10:47:59 -0300

    release: 0.0.4

--- src/App.tsx
+++ src/App.tsx
@@ -32,7 +32,7 @@ const App: React.FC = () => {
           <Routes />
         </AppProvider>
         <GlobalStyle />
-        <Version>0.0.3</Version>
+        <Version>0.0.4</Version>
       </ThemeProvider>
     </Router>
   );
